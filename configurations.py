27e26f11b5be6dc0aa2f203b5dd054ca3245c3be
Alex Yao <huangyu_yao@lolab-nthu.org>
2020-02-29 19:45:07 +0800

    Add DronVisualizer.py dependency.

---
+++ configurations.py
@@ -0,0 +1,35 @@
+arming_status_table = {
+    0: 'Init',
+    1: 'Standby',
+    2: 'Armed',
+    3: 'Standby Error',
+    4: 'Shutdown',
+    5: 'In air Restore',
+    6: '__Max__'
+    }
+
+flight_modes_table = {
+    0: 'Manual',
+    1: 'Altitude',
+    2: 'Position',
+    10: 'Acro',
+    14: 'Offboard',
+    15: 'Stabilized',
+    16: 'Rattitude',
+    #  AUTO-modes
+    3: 'Mission',
+    4: 'Loiter',
+    5: 'Return to Land',
+    6: 'RC Recovery',
+    7: 'Return to groundstation',
+    8: 'Land (engine fail)',
+    9: 'Land (GPS fail)',
+    12: 'Descend',
+    13: 'Terminate',
+    17: 'Takeoff',
+    18: 'Land',
+    19: 'Follow Target',
+    20: 'Precision Land',
+    21: 'Orbit',
+    22: '__Max__'
+    }
