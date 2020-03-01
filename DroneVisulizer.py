964b90ddc4f766e60de306b793e478b53353df41
Alex Yao <cktfg18nthu@gapp.nthu.edu.tw>
2020-03-01 21:41:27 +0800

    Add plot labels and time-series normalization option.

--- DroneVisulizer.py
+++ DroneVisulizer.py
@@ -12,13 +12,14 @@ from configurations import flight_modes_table, arming_status_table
 parser = argparse.ArgumentParser()
 parser.add_argument("input")
 parser.add_argument("-o", "--output", action="store_true")
+parser.add_argument("-0", "--start_from_zero", action="store_true")
 args = parser.parse_args()
 
 infile = args.input
 infileName = os.path.splitext(infile)[0]
 
 def load_log_file(filename):
-    useful_messages = ['vehicle_attitude', 'vehicle_local_position', 'vehicle_status']
+    useful_messages = ['estimator_status', 'vehicle_attitude', 'vehicle_local_position', 'vehicle_status']
     try:
         ulog = pyulog.ULog(filename, message_name_filter_list=useful_messages)
     except FileNotFoundError:
@@ -69,7 +70,11 @@ yaw = attitude.data['yaw']
 pitch = attitude.data['pitch']
 
 local_position = drone_log.get_dataset('vehicle_local_position')
-time_seq = np.array( [ microsec/1e6 for microsec in local_position.data['timestamp'] ] )
+if args.start_from_zero:
+    start_time = local_position.data['timestamp'][0]
+    time_seq = np.array( [ (microsec-start_time)/1e6 for microsec in local_position.data['timestamp'] ] )
+else:
+    time_seq = np.array( [ microsec/1e6 for microsec in local_position.data['timestamp'] ] )
 horizontal_std = np.array(local_position.data['eph'])
 vertical_std = np.array(local_position.data['epv'])
 horizontal_velocity_std = np.array(local_position.data['evh'])
@@ -96,27 +101,40 @@ fig = plt.figure(0)
 ax = fig.add_subplot(111, projection='3d')
 ax.invert_zaxis()
 ax.plot(x, y, z, 'green')
-ax.plot([x[0]], [y[0]], [z[0]], 'ro')
-ax.plot([x[-1]], [y[-1]], [z[-1]], 'bo')
+ax.plot([x[0]], [y[0]], [z[0]], 'bo', label="start")
+ax.plot([x[-1]], [y[-1]], [z[-1]], 'ro', label="end")
+ax.legend()
 fig, axs = plt.subplots(3, sharex=True)
+fig.suptitle("Local Position")
+axs[0].set_title("x")
 axs[0].plot(time_seq, x, color='g')
 axs[0].fill_between(time_seq, x-horizontal_std, x+horizontal_std, color='y', alpha=0.3)
-axs[0].plot(time_seq, sum_x, color='c')
+#axs[0].plot(time_seq, sum_x, color='c')
+axs[1].set_title("y")
 axs[1].plot(time_seq, y, color='g')
 axs[1].fill_between(time_seq, y-horizontal_std, y+horizontal_std, color='y', alpha=0.3)
-axs[1].plot(time_seq, sum_y, color='c')
+axs[1].set_ylabel("distance to reference point (m)")
+#axs[1].plot(time_seq, sum_y, color='c')
+axs[2].set_title("z")
 axs[2].invert_yaxis()
 axs[2].plot(time_seq, z, color='g')
 axs[2].fill_between(time_seq, z-vertical_std, z+vertical_std, color='y', alpha=0.3)
-axs[2].plot(time_seq, sum_z, color='c')
+#axs[2].plot(time_seq, sum_z, color='c')
+axs[2].set_xlabel("time (s)")
 fig, axs = plt.subplots(3, sharex=True)
+fig.suptitle("Local Velocity")
+axs[0].set_title("x")
 axs[0].plot(time_seq, vx)
 axs[0].fill_between(time_seq, vx-horizontal_velocity_std, vx+horizontal_velocity_std, color='r', alpha=0.3)
+axs[1].set_title("y")
 axs[1].plot(time_seq, vy)
 axs[1].fill_between(time_seq, vy-horizontal_velocity_std, vy+horizontal_velocity_std, color='r', alpha=0.3)
+axs[1].set_ylabel("velocity (m/s)")
+axs[2].set_title("z")
 axs[2].invert_yaxis()
 axs[2].plot(time_seq, vz)
 axs[2].fill_between(time_seq, vz-virtical_velocity_std, vz+virtical_velocity_std, color='r', alpha=0.3)
+axs[2].set_xlabel("time (s)")
 fig, axs = plt.subplots(3, sharex=True)
 axs[0].plot(time, roll, color='k')
 axs[1].plot(time, yaw, color='k')
